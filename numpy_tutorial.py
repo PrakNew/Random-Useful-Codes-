5b28f95d4a8afae95d4551d09edde1f59933c46c
PrakNew <prakharnewatia97@gmail.com>
2022-07-04 16:57:14 +0530

    Update numpy_tutorial.py

--- numpy_tutorial.py
+++ numpy_tutorial.py
@@ -17,3 +17,6 @@ A==B # here this means to check the values individually and return the array
 
 
 np.zeros(shape=(4, 4), dtype='int') #create an array of size 4*4 instead of shape we can also write [4,4]
+
+np.full((10,10),256,dtype=float) # create an array of size 10*10 having a fixed value like here 256 
+np.ones(shape=(10, 10), dtype='float') * 255 # this is equivalent to the above function 
