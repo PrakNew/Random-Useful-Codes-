68d2dff83919e3f73367918c3b65c183a3a607fa
Prakhar Newatia <40995798+PrakNew@users.noreply.github.com>
2023-01-16 14:14:46 +0530

    create mongodb tutorial file

---
+++ MongoDBtutorial.py
@@ -0,0 +1,34 @@
+import pymongo
+
+#Creating connections
+con_string = "mongodb://localhost:27017/"
+cluster = pymongo.MongoClient(con_string, connect=False)
+
+db = cluster['testdb']
+collection = db['testCollection']
+
+#Inserting the data
+data = {"_id":0,"name":"Prakhar","score":10}
+
+collection.insert_one(data)
+collection.insert_many([data,data]) #multiple insertion in one go 
+
+#Searching the data
+results = collection.find({}) # finds everything --> select * from db
+results = collection.find({"name":"Prakhar"})
+results = collection.find_one({"name":"Prakhar"})
+
+for result in results:
+    print(result)#Dictionary 
+
+#Deleting 
+results = collection.delete_one({}) #  deletes every data from the database
+results = collection.delete_one({"_id":1})
+results = collection.delete_many({"_id":1})
+
+#Updating
+results = collection.updata_one({"_id":5},{"$set":{"name":"tim"}})
+results = collection.updata_many({"_id":5},{"$set":{"name":"tim"}})
+
+#Counting
+post_count = collection.count_documents({}) #count all --> select count(*) from db
