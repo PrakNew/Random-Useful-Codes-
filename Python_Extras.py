55393275ad738c20971d5b114f31b3ef3cd54c42
Prakhar Newatia <40995798+PrakNew@users.noreply.github.com>
2023-01-29 08:45:48 +0530

    Update Python_Extras.py

--- Python_Extras.py
+++ Python_Extras.py
@@ -16,3 +16,10 @@ for x in range(5):
   l+=x,  #behaves like append
   
 vars(Object/Class) -> return a dictionary
+
+
+#OrderedDict 
+from collections import OrderedDict
+ord_dict = OrderedDict().fromkeys('GeeksForGeeks')
+ord_dict.popitem() #removes the last occuring item
+ord_dict.popitem(last = False) #Removes the first occuring item
