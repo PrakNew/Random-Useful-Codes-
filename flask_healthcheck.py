7515f23cddb563e2ffd2e40df6c34693870bac03
Prakhar Newatia <40995798+PrakNew@users.noreply.github.com>
2023-08-19 16:34:44 +0530

    Update flask_healthcheck.py

--- flask_healthcheck.py
+++ flask_healthcheck.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+from flask_pydantic import validate
+from pydantic import BaseModel
 
 app = Flask(__name__)
 CORS(app)
@@ -13,9 +15,35 @@ app.config["TIMEOUT"] = 60000000
 app.config.from_mapping(app_config)
 
 
+class SearchQuery(BaseModel):
+    query: str
+
+
+class QueryModel(BaseModel):
+    age: int
+
+
+class ResponseModel(BaseModel):
+    id: int
+    age: int
+    name: str
+    nickname: str
+
+
+# Example 1: query parameters only
+@app.route("/", methods=["GET"])
+@validate()
+def get(query: QueryModel):
+    age = query.age
+    return ResponseModel(age=age, id=0, name="abc", nickname="123")
+
+
 @app.route("/healthcheck/app", methods=["GET"])
+@validate()
 def healthcheck_status():
     try:
+        # print("REQUEST: ", query.query)
+        # print("QUERY", query)
         return '{"m":"success","s":0,"d":[]}'
     except Exception as e:
         return "error"
