d1f50c93c0cb6775a6076f4064259f7696ef4142
Prakhar Newatia <40995798+PrakNew@users.noreply.github.com>
2023-12-23 16:27:37 +0530

    plotting the first plot diagram

---
+++ Data Science/line_plots.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt 
+
+x_days = [1,2,3,4,5]
+
+y_price1 = [9,9.5,10.5,10,12]
+y_price2 = [11,12,10.5,11.5,12.5]
+
+
+plt.title("Stock Movement")
+plt.xlabel("week days")
+plt.ylabel("Price in USD")
+
+
+
+plt.plot(x_days,y_price1,label = "Stock 1")
+
+plt.legend(loc=2, fontsize=12)
+
+plt.show()
